fec731c025b958ca7a6335239ccf80fecaba19a6
LilyPol <pollypin33@gmail.com>
2018-10-01 21:36:59 +0300

    lesson 6

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import moment from 'moment';
 
 import StatusBar from 'components/StatusBar';
 import Composer from 'components/Composer';
@@ -6,22 +7,41 @@ import Post from 'components/Post';
 import Spinner from 'components/Spinner';
 
 import Styles from './styles.m.css';
+import {getUniqueID} from 'instruments';
 
 export default class Feed extends Component {
+    constructor(){
+        super();
+
+        this._createPost = this._createPost.bind(this);
+    }
+
     state = {
         posts: [
             {id: '123', comment: 'Hi there!', created: 1526825076849},
             {id: '456', comment: 'Приветик!', created: 1526825077500} 
         ],
-        showSpinning: false //
-    };   
+        isPostsFetching: false
+    };
+    
+    _createPost(comment){
+        const post = {
+            id: getUniqueID(),
+            created: moment.now(),
+            comment,
+        };
+
+        this.setState(({posts}) =>({
+            posts: [post, ...posts],
+        }));
+    }
     
     render() {
         const {posts} = this.state; 
-        const {showSpinning} = this.state; //
+        const {isPostsFetching} = this.state;
         
-        console.log('1 Feed this.state=',this.state);        
-        console.log('1 Feed isSpinning=',showSpinning)
+        /*console.log('1 Feed this.state=',this.state);        
+        console.log('1 Feed isSpinning=',isPostsFetching)*/
 
         const postsJSX = posts.map((post) => {
             //console.log('Feed post.isSpinning=',post.isSpinning);
@@ -29,18 +49,18 @@ export default class Feed extends Component {
         });
 
         //      
-        console.log('2 Feed isSpinning=',this.state.showSpinning)
+        /* console.log('2 Feed isSpinning=',this.state.isPostsFetching)
         setTimeout(() => this.setState({
-            showSpinning: !this.state.showSpinning
+            isPostsFetching: !this.state.isPostsFetching
         }), 5000)
-        console.log('3 Feed isSpinning=',this.state.showSpinning)                      
+        console.log('3 Feed isSpinning=',this.state.isPostsFetching) */                      
         //
 
         return (        
         <section className = {Styles.feed}>        
-            <Spinner isSpinning = {showSpinning} />            
+            <Spinner isSpinning = {isPostsFetching} />            
             <StatusBar />            
-            <Composer />
+            <Composer _createPost = {this._createPost} />
             {postsJSX}
         </section>
         );
