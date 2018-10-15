6fbca7d3c69d6c445b53c70fed06718d5646676b
LilyPol <pollypin33@gmail.com>
2018-10-15 12:11:42 +0300

    lesson-10

--- source/components/Feed/index.js
+++ source/components/Feed/index.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import { Transition } from 'react-transition-group';
+import { 
+    Transition, 
+    CSSTransition, 
+    TransitionGroup 
+} from 'react-transition-group';
 import { fromTo } from 'gsap';
 
 import { withProfile } from 'components/HOC/withProfile';
@@ -183,13 +187,24 @@ export default class Feed extends Component {
 
         const postsJSX = posts.map((post) => {            
             return (
-                <Catcher key = { post.id }>
-                    <Post                         
-                        { ...post }                         
-                        _likePost = { this._likePost }
-                        _removePost = { this._removePost } 
-                    />
-                </Catcher>
+                <CSSTransition
+                    classNames = { {
+                        enter:       Styles.postInStart,
+                        enterActive: Styles.postInEnd,
+                    } } 
+                    key = { post.id }
+                    timeout = { {
+                        enter: 500, 
+                        exit:  400,
+                    } }>
+                    <Catcher>
+                        <Post                         
+                            { ...post }                         
+                            _likePost = { this._likePost }
+                            _removePost = { this._removePost } 
+                        />
+                    </Catcher>
+                </CSSTransition>
             );
         });
 
@@ -213,7 +228,7 @@ export default class Feed extends Component {
                     >                
                     <Postman _createPost = { this._createPost } />
                 </Transition>                
-                {postsJSX}
+                <TransitionGroup>{postsJSX}</TransitionGroup>     
             </section>
         );
     }
