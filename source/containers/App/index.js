e945b2e2058b2f376410cc8dc9842aabc5f30a85
LilyPol <pollypin33@gmail.com>
2018-10-29 22:33:49 +0200

    lesson-13

--- source/containers/App/index.js
+++ source/containers/App/index.js
@@ -1,9 +1,11 @@
 // Core
 import React, { Component } from 'react';
+import { hot } from 'react-hot-loader';
 
 //Components
 import Catcher from 'components/Catcher';
 import Feed from 'components/Feed';
+import Profile from 'components/Profile';
 import { Provider } from 'components/HOC/withProfile';
 
 //Instruments
@@ -15,12 +17,14 @@ const options = {
     currentUserLastName:  'Полежаева',//'Вакациенко',
 };
 
+@hot(module)
 export default class App extends Component {
     render () {
         return (
             <Catcher>
                 <Provider value = { options }>
                     <Feed />
+                    <Profile />
                 </Provider>
             </Catcher>
         );
